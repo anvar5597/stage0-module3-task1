3411c0bb6f6da09432d7465684c9aedd57e5afd1
Anvar <104772445+anvar5597@users.noreply.github.com>
2022-08-27 10:02:21 +0500

    Update PrintWordsApp.java

--- src/main/java/lang/print/gaps/task1/PrintWordsApp.java
+++ src/main/java/lang/print/gaps/task1/PrintWordsApp.java
@@ -3,7 +3,7 @@ package lang.print.gaps.task1;
 public class PrintWordsApp {
     public static void main(String[] args) {
         //write your code here
-        System.out.println("java is smth");
+        System.out.println("java \nis \nsmth");
         
      
     }
